523b5f20677a24fb0f5966484f3cbdf15d9788a4
admin <you@example.com>
2021-12-16 14:49:34 +0100

    basics

---
+++ main.js
@@ -0,0 +1,23 @@
+var myFirstVariable;
+
+var myApple //normal
+var myapple //Camel Case most used!!
+var MyApple //Pascal Case
+
+myFirstVariable = 5;
+myFirstVariable = undefined;
+
+console.log ("Hello World");
+
+document.getElementById('warning').innerHTML = "2"+"3";
+console.log('2'+'3');
+document.getElementById('red1').innerHTML = '4'+ ' ' +'5'; 
+console.log(2+3);
+
+
+/* document.getElementsByClassName('blue').innerHTML = "4"+"3";
+console.log('2'+'3')
+ */
+/* 20 is a text
+"20" this is a number with a string
+"twenty" this is a text */
